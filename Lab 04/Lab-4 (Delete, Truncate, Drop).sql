cdce7c6d8f5fbe1a0732c8edf5503b4686e630bb
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-09 18:31:34 +0530

    9-7-24

--- Lab 04/Lab-4 (Delete, Truncate, Drop).sql
+++ Lab 04/Lab-4 (Delete, Truncate, Drop).sql
@@ -40,7 +40,7 @@ CREATE TABLE EMPLOYEE_MASTER(
 );
 
 INSERT INTO EMPLOYEE_MASTER (EmpNo, EmpName, Joining_Date, Salary, City) 
-VALUES  (101, 'Keyur', '2002-01-05', 12000.00, 'Rajkot'),
+VALUES (101, 'Keyur', '2002-01-05', 12000.00, 'Rajkot'),
 		(102, 'Hardik', '2004-02-15', 14000.00, 'Ahmedabad'),
 		(103, 'Kajal', '2006-03-14', 15000.00, 'Baroda'),
 		(104, 'Bhoomi', '2005-06-23', 12500.00, 'Ahmedabad'),
